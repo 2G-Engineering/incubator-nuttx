6260f9f22cdf6d6c8e50885fa3afbc69602c713a
Joshua Lange <jlange@2g-eng.com>
2020-02-20 15:41:43 -0600

    Finished HT16K33 driver.

--- drivers/leds/ht16k33.c
+++ drivers/leds/ht16k33.c
@@ -65,11 +65,14 @@ struct ht16k33_dev_s
  * Private Function Prototypes
  ****************************************************************************/
 
-static int ht16k33_i2c_write_byte(FAR struct ht16k33_dev_s *priv,
+static int ht16k33_i2c_write_reg(FAR struct ht16k33_dev_s *priv,
                                     uint8_t const reg_addr,
                                     uint8_t const reg_val);
-static int ht16k33_set_led_mode(FAR struct ht16k33_dev_s *priv,
-                                  uint8_t const led_out_x_mode);
+static int ht16k33_i2c_write_data(FAR struct ht16k33_dev_s *priv,
+                                    uint8_t const reg_addr,
+                                    uint8_t const reg_val,
+                                    FAR const char *data,
+                                    uint32_t const data_len);
 
 static int ht16k33_open(FAR struct file *filep);
 static int ht16k33_close(FAR struct file *filep);
@@ -99,26 +102,41 @@ static const struct file_operations g_ht16k33_fileops =
  ****************************************************************************/
 
 /****************************************************************************
- * Name: ht16k33_i2c_write_byte
+ * Name: ht16k33_i2c_write_data
  *
  * Description:
- *   Write a single byte to one of the HT16K33 configuration registers.
+ *   Write multiple bytes to the HT16K33 device.
  *
  ****************************************************************************/
 
-static int ht16k33_i2c_write_byte(FAR struct ht16k33_dev_s *priv,
+static int ht16k33_i2c_write_data(FAR struct ht16k33_dev_s *priv,
                                     uint8_t const reg_addr,
-                                    uint8_t const reg_val)
+                                    uint8_t const reg_val,
+                                    FAR const char *data,
+                                    uint32_t data_len)
 {
   struct i2c_config_s config;
   int ret;
 
-  /* assemble the 2 byte message comprised of reg_addr and reg_val */
+  /* Can't send more data than our device's RAM size */
+  if (data_len > HT16K33_DISP_RAM_SIZE)
+    {
+      data_len = HT16K33_DISP_RAM_SIZE;
+    }
 
-  uint8_t const BUFFER_SIZE = 1;
+  /* Assemble the message comprised of reg_addr and reg_val, plus
+   * the additional data we want to send. */
+
+  uint8_t const BUFFER_SIZE = HT16K33_DISP_RAM_SIZE + 1;
   uint8_t buffer[BUFFER_SIZE];
 
-  buffer[0] = reg_addr | reg_val;
+  buffer[0] = reg_addr & HT16K33_REG_ADDR_MASK;
+  buffer[0] |= reg_val & HT16K33_REG_DATA_MASK;
+
+  if (data)
+    {
+      memcpy(&buffer[1], data, data_len);
+    }
 
   /* Setup up the I2C configuration */
 
@@ -128,13 +146,60 @@ static int ht16k33_i2c_write_byte(FAR struct ht16k33_dev_s *priv,
 
   /* Write the register address followed by the data (no RESTART) */
 
-  ledinfo("i2c addr: 0x%02X reg addr: 0x%02X value: 0x%02X\n", priv->i2c_addr,
-          reg_addr, reg_val);
+  ledinfo("i2c addr: 0x%02X reg addr: 0x%02X value: 0x%02X (%d bytes additional)\n",
+          priv->i2c_addr, reg_addr, reg_val, data_size);
 
   ret = i2c_write(priv->i2c, &config, buffer, BUFFER_SIZE);
   if (ret < 0)
     {
       lederr("ERROR: i2c_write returned error code %d\n", ret);
+      return ret;
+    }
+
+  return data_len;
+}
+
+
+/****************************************************************************
+ * Name: ht16k33_i2c_write_reg
+ *
+ * Description:
+ *   Write a single byte to one of the HT16K33 configuration registers.
+ *
+ ****************************************************************************/
+
+static int ht16k33_i2c_write_reg(FAR struct ht16k33_dev_s *priv,
+                                    uint8_t const reg_addr,
+                                    uint8_t const reg_val)
+{
+  return ht16k33_i2c_write_data(priv, reg_addr, reg_val, NULL, 0);
+}
+
+/****************************************************************************
+ * Name: ht16k33_update_blink_onoff
+ *
+ * Description:
+ *   Update the display setup register, which controls global LED enable
+ *   and global blink settings.
+ *
+ ****************************************************************************/
+
+static int ht16k33_update_blink_onoff(FAR struct ht16k33_dev_s *priv)
+{
+  int ret = -1;
+  uint8_t disp_setup = 0;
+
+  if (priv->on)
+    {
+      disp_setup |= HT16K33_DISP_ON;
+    }
+
+  disp_setup |= priv->blink;
+
+  ret = ht16k33_i2c_write_reg(priv, HT16K33_DISP, disp_setup);
+  if (ret < 0)
+    {
+      lederr("ERROR: Could not set HT16K33 display options.\n");
       return ret;
     }
 
@@ -147,33 +212,123 @@ static int ht16k33_i2c_write_byte(FAR struct ht16k33_dev_s *priv,
  * Description:
  *   Enables or disables all LEDs at once.
  *
+ * Input Parameters:
+ *   priv       - Reference to an instance of the ht16k33 device
+ *   on         - If true, all LEDs will be enabled.  If false, all
+ *                LEDs will be disabled.
+ *
  ****************************************************************************/
 
 static int ht16k33_set_led_onoff(FAR struct ht16k33_dev_s *priv,
                                       bool const on)
 {
-  int ret = -1;
+  int ret;
   priv->on = on;
 
   ret = ht16k33_update_blink_onoff(priv);
 
   return ret;
 }
+
 /****************************************************************************
  * Name: ht16k33_set_led_blink_rate
  *
  * Description:
  *   Set the blink rate for all LEDs
  *
+ * Input Parameters:
+ *   priv       - Reference to an instance of the ht16k33 device
+ *   blinkrate  - one of HT16K33_DISP_BLINKOFF, HT16K33_DISP_BLINK2HZ,
+ *                HT16K33_DISP_BLINK1HZ, or HT16K33_DISP_BLINK05HZ
+ *
  ****************************************************************************/
 
 static int ht16k33_set_led_blink_rate(FAR struct ht16k33_dev_s *priv,
                                       uint8_t const blinkrate)
 {
-  int ret = -1;
+  int ret;
   priv->blink = blinkrate;
 
   ret = ht16k33_update_blink_onoff(priv);
+
+  return ret;
+}
+
+/****************************************************************************
+ * Name: ht16k33_set_brightness
+ *
+ * Description:
+ *   Set the brightness for all LEDs
+ *
+ * Input Parameters:
+ *   priv       - Reference to an instance of the ht16k33 device
+ *   brightness - Brightness in the range [0, 15]
+ *
+ * Returned Value:
+ *   Zero (OK) on success; a negated errno value on failure.
+ *
+ ****************************************************************************/
+
+static int ht16k33_set_led_brightness(FAR struct ht16k33_dev_s *priv,
+                                      uint8_t brightness)
+{
+  int ret;
+
+  if (brightness > HT16K33_DIM_MAX)
+    {
+      brightness = HT16K33_DIM_MAX;
+    }
+
+  ret = ht16k33_i2c_write_reg(priv, HT16K33_DIM, brightness);
+  if (ret < 0)
+    {
+      lederr("ERROR: Could not set HT16K33 brightness.\n");
+      return ret;
+    }
+
+  return OK;
+}
+
+/****************************************************************************
+ * Name: ht16k33_set_standby
+ *
+ * Description:
+ *   Sets the standby state of the HT16K33 device.
+ *
+ * Input Parameters:
+ *   priv       - Reference to an instance of the ht16k33 device
+ *   standby    - if true, device will enter standby.  If false,
+ *                device will enter normal operation mode.
+ *
+ * Returned Value:
+ *   Zero (OK) on success; a negated errno value on failure.
+ *
+ ****************************************************************************/
+
+static int ht16k33_set_standby(FAR struct ht16k33_dev_s *priv,
+                                      bool const standby)
+{
+  int ret;
+  uint8_t system_setup = 0;
+
+  if (!standby)
+    {
+      system_setup |= HT16K33_SS_NORMAL;
+    }
+
+  ret = ht16k33_i2c_write_reg(priv, HT16K33_SS, system_setup);
+  if (ret < 0)
+    {
+      lederr("ERROR: Could not set HT16K33 standby.\n");
+      return ret;
+    }
+  else
+    {
+      /* Allow 25ms (~2 frame intervals) for the internal
+       * oscillator to change state */
+
+      nxsig_usleep(25 * USEC_PER_MSEC);
+    }
 
   return OK;
 }
@@ -190,47 +345,41 @@ static int ht16k33_open(FAR struct file *filep)
 {
   FAR struct inode *inode = filep->f_inode;
   FAR struct ht16k33_dev_s *priv = inode->i_private;
-  int ret = -1;
+  int ret;
 
   /* Wake up the HT16K33 by enabling the oscillator. */
 
-  ret = ht16k33_i2c_write_byte(priv, HT16K33_SS, HT16K33_SS_NORMAL);
+  ret = ht16k33_set_standby(priv, false);
   if (ret < 0)
     {
-      lederr("ERROR: Could not set initial config for HT16K33_SS\n");
+      lederr("ERROR: Could not set HT16K33 standby mode\n");
       return ret;
     }
 
-  /* Allow 25ms (~2 frame intervals) for the internal oscillator to come up */
-
-  nxsig_usleep(25 * USEC_PER_MSEC);
-
   /* Configure the HT16K33 ROW/INT pin to row output mode */
 
-  ret = ht16k33_i2c_write_byte(priv, HT16K33_RIS, HT16K33_RIS_OUT);
+  ret = ht16k33_i2c_write_reg(priv, HT16K33_RIS, HT16K33_RIS_OUT);
   if (ret < 0)
     {
       lederr("ERROR: Could not set initial config for HT16K33_RIS\n");
       return ret;
     }
 
-
-  /* Enable LEDs & set blink to off */
-
-  ret = ht16k33_i2c_write_byte(priv, HT16K33_DISP,
-                               HT16K33_DISP_ON | HT16K33_DISP_BLINKOFF);
+  /* Set LEDs to full brightness */
+  ret = ht16k33_set_led_brightness(priv, HT16K33_DIM_MAX);
   if (ret < 0)
     {
-      lederr("ERROR: Could not set initial config for HT16K33_DISP\n");
+      lederr("ERROR: Could not set HT16K33 brightness\n");
       return ret;
     }
 
-  /* Set LEDs to full brightness */
-
-  ret = ht16k33_i2c_write_byte(priv, HT16K33_DIM, HT16K33_DIM_MAX);
+  /* Enable LEDs & set blink to off */
+  priv->blink = HT16K33_DISP_BLINKOFF;
+  priv->on = true;
+  ret = ht16k33_update_blink_onoff(priv);
   if (ret < 0)
     {
-      lederr("ERROR: Could not set initial config for HT16K33_DISP\n");
+      lederr("ERROR: Could not update HT16K33 system setup\n");
       return ret;
     }
 
@@ -249,28 +398,23 @@ static int ht16k33_close(FAR struct file *filep)
 {
   FAR struct inode *inode = filep->f_inode;
   FAR struct ht16k33_dev_s *priv = inode->i_private;
-  int ret = -1;
+  int ret;
 
   /* Turn all led drivers off */
 
-  ret = ht16k33_i2c_write_byte(priv, HT16K33_DISP,
-                               HT16K33_DISP_OFF | HT16K33_DISP_BLINKOFF);
+  ret = ht16k33_set_led_onoff(priv, false);
   if (ret < 0)
     {
-      lederr("ERROR: Could not set HT16K33_DISP off\n");
+      lederr("ERROR: Could not set HT16K33 LEDs to off\n");
       return ret;
     }
 
   /* Send the HT16K33 back to standby mode */
 
-  ret = ht16k33_i2c_write_byte(priv, HT16K33_SS, HT16K33_SS_STANDBY);
+  ret = ht16k33_set_standby(priv, false);
   if (ret < 0)
     {
-      lederr("ERROR: Could not set HT16K33_SS to standby\n");
-      return ret;
-    }
-  if (ret < 0)
-    {
+      lederr("ERROR: Could not set HT16K33 standby mode\n");
       return ret;
     }
 
@@ -295,21 +439,43 @@ static int ht16k33_ioctl(FAR struct file *filep, int cmd, unsigned long arg)
 
   switch (cmd)
     {
-      /* Set the brightness of an indivual LED. Arg: ht16k33_led_brightness_s
-       * pointer.
-       */
+      /* Set the brightness of all LEDs. Arg: brightness (0-15) */
 
     case PWMIOC_SETLED_BRIGHTNESS:
       {
-        /* Retrieve the information handed over as argument for this ioctl */
 
-        FAR const uint8_t brightness = *((uint8_t*)arg);
+        /* Set the brightness of the led */
 
-        DEBUGASSERT(ptr != NULL);
+        ret = ht16k33_set_led_brightness(priv, (uint8_t) arg);
+      }
+      break;
 
-        /* Set the brighntess of the led */
+      /* Set the global on/off state of the LEDs. Arg: 0 = off / 1 = on */
 
-//        ret = ht16k33_i2c_write_byte(priv, ptr->led, ptr->brightness);
+    case PWMIOC_SETLED_ONOFF:
+      {
+
+        ret = ht16k33_set_led_onoff(priv, (bool) arg);
+      }
+      break;
+
+      /* Set the standby state of the device. Arg: 0 = run / 1 = standby */
+
+    case PWMIOC_SETLED_STANDBY:
+      {
+
+        ret = ht16k33_set_standby(priv, (bool) arg);
+      }
+      break;
+
+      /* Set the global blink rate for the LEDs.
+       * Arg: one of HT16K33_DISP_BLINKOFF, HT16K33_DISP_BLINK2HZ,
+       * HT16K33_DISP_BLINK1HZ, HT16K33_DISP_BLINK05HZ */
+
+    case PWMIOC_SETLED_BLINK:
+      {
+
+        ret = ht16k33_set_led_blink_rate(priv, (uint8_t)arg);
       }
       break;
 
@@ -324,6 +490,47 @@ static int ht16k33_ioctl(FAR struct file *filep, int cmd, unsigned long arg)
     }
 
   return ret;
+}
+
+/****************************************************************************
+ * Name: ht16k33_read
+ ****************************************************************************/
+
+static ssize_t ht16k33_read(FAR struct file *filep, FAR char *buffer, size_t buflen)
+{
+    /* Revisit: This device can also read switches;
+     * read could return switch data */
+  return -ENOSYS;
+}
+
+/****************************************************************************
+ * Name: ht16k33_write
+ ****************************************************************************/
+
+static ssize_t ht16k33_write(FAR struct file *filep, FAR const char *buffer,
+                          size_t buflen)
+{
+  FAR struct inode *inode = filep->f_inode;
+  FAR struct ht16k33_dev_s *priv = inode->i_private;
+  int written;
+
+  if (buffer == NULL)
+    {
+      lederr("ERROR: Buffer is null\n");
+      return -1;
+    }
+
+  if (buflen < 1)
+    {
+      lederr("ERROR: At least 1 byte required!\n");
+      return -1;
+    }
+
+  /* Write to display RAM, always restarting from address 0 */
+
+  written = ht16k33_i2c_write_data(priv, HT16K33_DDAP, 0, buffer, buflen);
+
+  return written;
 }
 
 /****************************************************************************
